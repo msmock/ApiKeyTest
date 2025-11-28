576a1ab3ae6f34f4a1ab58822fc49027fec519e4
Martin Smock <martin_smock@me.com>
2025-11-28 12:47:31 +0100

    Initial commit

---
+++ src/main/java/org/fnm/apikeytest/ApiKeyTestApplication.java
@@ -0,0 +1,11 @@
+package org.fnm.apikeytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKeyTestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiKeyTestApplication.class, args);
+	}
+}
---
+++ src/main/java/org/fnm/apikeytest/security/AuthenticationService.java
@@ -0,0 +1,23 @@
+package org.fnm.apikeytest.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+/**
+ * verify the api key send in the request header against the registered ones.
+ */
+public class AuthenticationService {
+
+    private static final String AUTH_TOKEN_HEADER_NAME = "X-API-KEY";
+    private static final String AUTH_TOKEN = "Baeldung";
+
+    public static Authentication getAuthentication(HttpServletRequest request) {
+        String apiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
+        if (apiKey == null || !apiKey.equals(AUTH_TOKEN)) {
+            throw new BadCredentialsException("Invalid API Key");
+        }
+        return new ApiKeyAuthentication(apiKey, AuthorityUtils.NO_AUTHORITIES);
+    }
+}

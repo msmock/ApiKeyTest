d6be6ec9fb3ce6fddcaf48f171b06319ce5a2dc9
Martin Smock <martin_smock@me.com>
2025-11-28 15:02:12 +0100

    used just a filter and keys in application properties

--- src/main/java/org/fnm/apikeytest/controller/ResourceController.java
+++ src/main/java/org/fnm/apikeytest/controller/ResourceController.java
@@ -1,13 +1,19 @@
 package org.fnm.apikeytest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ResourceController {
 
     @GetMapping("/home")
-    public String homeEndpoint() {
+    public String get() {
+        return "Return the requested resource!";
+    }
+
+    @PostMapping("/home")
+    public String post() {
         return "Return the requested resource!";
     }
 
---
+++ src/main/java/org/fnm/apikeytest/security/ApiKeyFilter.java
@@ -0,0 +1,44 @@
+package org.fnm.apikeytest.security;
+
+import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Set;
+
+@Component
+@Order(1)
+public class ApiKeyFilter implements Filter {
+
+    @Value( "${security.api-keys}" )
+    private Set<String> registeredKeys;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        String method =  req.getMethod();
+
+        String apiKey = req.getHeader("X-API-KEY");
+        System.out.println("API Key : " + apiKey);
+
+        if (method.equals("GET")){
+            chain.doFilter(request, response);
+        } else if (apiKey != null && registeredKeys.contains(apiKey)){
+            chain.doFilter(request, response);
+        } else {
+            res.setStatus(405);
+            res.getWriter().write("Method call not authorized!");
+        }
+    }
+}
